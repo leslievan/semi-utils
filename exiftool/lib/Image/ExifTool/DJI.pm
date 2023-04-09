50a30b466db25ceaf22a70045aab5a055c709441
Leslie Van <viruswjc@gmail.com>
2023-04-09 22:08:45 +0800

    引入 exiftool 可执行文件

---
+++ exiftool/lib/Image/ExifTool/DJI.pm
@@ -0,0 +1,249 @@
+#------------------------------------------------------------------------------
+# File:         DJI.pm
+#
+# Description:  DJI Phantom maker notes tags
+#
+# Revisions:    2016-07-25 - P. Harvey Created
+#               2017-06-23 - PH Added XMP tags
+#------------------------------------------------------------------------------
+
+package Image::ExifTool::DJI;
+
+use strict;
+use vars qw($VERSION);
+use Image::ExifTool qw(:DataAccess :Utils);
+use Image::ExifTool::Exif;
+use Image::ExifTool::XMP;
+use Image::ExifTool::GPS;
+
+$VERSION = '1.08';
+
+sub ProcessDJIInfo($$$);
+
+my %convFloat2 = (
+    PrintConv => 'sprintf("%+.2f", $val)',
+    PrintConvInv => '$val',
+);
+
+# DJI maker notes (ref PH, mostly educated guesses based on DJI QuickTime::UserData tags)
+%Image::ExifTool::DJI::Main = (
+    WRITE_PROC => \&Image::ExifTool::Exif::WriteExif,
+    CHECK_PROC => \&Image::ExifTool::Exif::CheckExif,
+    GROUPS => { 0 => 'MakerNotes', 2 => 'Camera' },
+    NOTES => q{
+        This table lists tags found in the maker notes of images from some DJI
+        Phantom drones.
+    },
+    0x01 => { Name => 'Make',       Writable => 'string' },
+  # 0x02 - int8u[4]: "1 0 0 0", "1 1 0 0"
+    0x03 => { Name => 'SpeedX',     Writable => 'float', %convFloat2 }, # (guess)
+    0x04 => { Name => 'SpeedY',     Writable => 'float', %convFloat2 }, # (guess)
+    0x05 => { Name => 'SpeedZ',     Writable => 'float', %convFloat2 }, # (guess)
+    0x06 => { Name => 'Pitch',      Writable => 'float', %convFloat2 },
+    0x07 => { Name => 'Yaw',        Writable => 'float', %convFloat2 },
+    0x08 => { Name => 'Roll',       Writable => 'float', %convFloat2 },
+    0x09 => { Name => 'CameraPitch',Writable => 'float', %convFloat2 },
+    0x0a => { Name => 'CameraYaw',  Writable => 'float', %convFloat2 },
+    0x0b => { Name => 'CameraRoll', Writable => 'float', %convFloat2 },
+);
+
+# DJI debug maker notes
+%Image::ExifTool::DJI::Info = (
+    PROCESS_PROC => \&ProcessDJIInfo,
+    GROUPS => { 0 => 'MakerNotes', 2 => 'Camera' },
+    NOTES => 'Tags written by some DJI drones.',
+    VARS => { LONG_TAGS => 2 },
+    ae_dbg_info         => { Name => 'AEDebugInfo' },
+    ae_histogram_info   => { Name => 'AEHistogramInfo' },
+    ae_local_histogram  => { Name => 'AELocalHistogram' },
+    ae_liveview_histogram_info  => { Name => 'AELiveViewHistogramInfo' },
+    ae_liveview_local_histogram => { Name => 'AELiveViewLocalHistogram' },
+    awb_dbg_info        => { Name => 'AWBDebugInfo' },
+    af_dbg_info         => { Name => 'AFDebugInfo' },
+    hiso                => { Name => 'Histogram' },
+    xidiri              => { Name => 'Xidiri' },
+   'GimbalDegree(Y,P,R)'=> { Name => 'GimbalDegree' },
+   'FlightDegree(Y,P,R)'=> { Name => 'FlightDegree' },
+    adj_dbg_info        => { Name => 'ADJDebugInfo' },
+    sensor_id           => { Name => 'SensorID' },
+   'FlightSpeed(X,Y,Z)' => { Name => 'FlightSpeed' },
+    hyperlapse_dbg_info => { Name => 'HyperlapsDebugInfo' },
+);
+
+# thermal parameters in APP4 of DJI ZH20T images (ref forum11401)
+%Image::ExifTool::DJI::ThermalParams = (
+    PROCESS_PROC => \&Image::ExifTool::ProcessBinaryData,
+    GROUPS => { 0 => 'APP4', 2 => 'Image' },
+    NOTES => 'Thermal parameters extracted from APP4 of DJI RJPEG files from the ZH20T.',
+  # 0x00 - 0xaa551206 - temperature header magic number 
+    0x24 => { Name => 'K1', Format => 'float' },
+    0x28 => { Name => 'K2', Format => 'float' },
+    0x2c => { Name => 'K3', Format => 'float' },
+    0x30 => { Name => 'K4', Format => 'float' },
+    0x34 => { Name => 'KF', Format => 'float' },
+    0x38 => { Name => 'B1', Format => 'float' },
+    0x3c => { Name => 'B2', Format => 'float' },
+    0x44 => { Name => 'ObjectDistance',     Format => 'int16u' },
+    0x46 => { Name => 'RelativeHumidity',   Format => 'int16u' },
+    0x48 => { Name => 'Emissivity',         Format => 'int16u' },
+    0x4a => { Name => 'Reflection',         Format => 'int16u',  },
+    0x4c => { Name => 'AmbientTemperature', Format => 'int16u' }, # (aka D1)
+    0x50 => { Name => 'D2', Format => 'int32s' },
+    0x54 => { Name => 'KJ', Format => 'int16u' },
+    0x56 => { Name => 'DB', Format => 'int16u' },
+    0x58 => { Name => 'KK', Format => 'int16u' },
+  # 0x500 - 0x55aa1206 - device header magic number 
+    # (nothing yet decoded from device header)
+);
+
+# thermal parameters in APP4 of DJI M3T, H20N, M2EA and some M30T images (ref PH/forum11401)
+%Image::ExifTool::DJI::ThermalParams2 = (
+    PROCESS_PROC => \&Image::ExifTool::ProcessBinaryData,
+    GROUPS => { 0 => 'APP4', 2 => 'Image' },
+    NOTES => 'Thermal parameters extracted from APP4 of DJI M3T RJPEG files.',
+    0x00 => { Name => 'AmbientTemperature',  Format => 'float', PrintConv => 'sprintf("%.1f C",$val)' }, # (NC)
+    0x04 => { Name => 'ObjectDistance',      Format => 'float', PrintConv => 'sprintf("%.1f m",$val)' },
+    0x08 => { Name => 'Emissivity',          Format => 'float', PrintConv => 'sprintf("%.2f",$val)' },
+    0x0c => { Name => 'RelativeHumidity',    Format => 'float', PrintConv => 'sprintf("%g %%",$val*100)' },
+    0x10 => { Name => 'ReflectedTemperature',Format => 'float', PrintConv => 'sprintf("%.1f C",$val)' },
+    0x65 => { Name => 'IDString',            Format => 'string[16]' }, # (NC)
+);
+
+# thermal parameters in APP4 of some DJI M30T images (ref PH)
+%Image::ExifTool::DJI::ThermalParams3 = (
+    PROCESS_PROC => \&Image::ExifTool::ProcessBinaryData,
+    GROUPS => { 0 => 'APP4', 2 => 'Image' },
+    NOTES => 'Thermal parameters extracted from APP4 of some DJI RJPEG files.',
+  # 0x00 - 0xaa553800 - params3 magic number 
+    0x04 => { Name => 'RelativeHumidity',    Format => 'int16u' },
+    0x06 => { Name => 'ObjectDistance',      Format => 'int16u', ValueConv => '$val / 10' },
+    0x08 => { Name => 'Emissivity',          Format => 'int16u', ValueConv => '$val / 100' },
+    0x0a => { Name => 'ReflectedTemperature',Format => 'int16u', ValueConv => '$val / 10' },
+);
+
+%Image::ExifTool::DJI::XMP = (
+    %Image::ExifTool::XMP::xmpTableDefaults,
+    GROUPS => { 0 => 'XMP', 1 => 'XMP-drone-dji', 2 => 'Location' },
+    NAMESPACE => 'drone-dji',
+    TABLE_DESC => 'XMP DJI',
+    VARS => { NO_ID => 1 },
+    NOTES => 'XMP tags used by DJI for images from drones.',
+    AbsoluteAltitude  => { Writable => 'real' },
+    RelativeAltitude  => { Writable => 'real' },
+    GimbalRollDegree  => { Writable => 'real' },
+    GimbalYawDegree   => { Writable => 'real' },
+    GimbalPitchDegree => { Writable => 'real' },
+    FlightRollDegree  => { Writable => 'real' },
+    FlightYawDegree   => { Writable => 'real' },
+    FlightPitchDegree => { Writable => 'real' },
+    GpsLatitude => {
+        Name => 'GPSLatitude',
+        Writable => 'real',
+        Avoid => 1,
+        PrintConv    => 'Image::ExifTool::GPS::ToDMS($self, $val, 1, "N")',
+        PrintConvInv => 'Image::ExifTool::GPS::ToDegrees($val, 1, "lat")',
+    },
+    GpsLongtitude => { # (sic)
+        Name => 'GPSLongtitude',
+        Writable => 'real',
+        PrintConv    => 'Image::ExifTool::GPS::ToDMS($self, $val, 1, "E")',
+        PrintConvInv => 'Image::ExifTool::GPS::ToDegrees($val, 1, "lon")',
+    },
+    GpsLongitude => { #PH (NC)
+        Name => 'GPSLongitude',
+        Writable => 'real',
+        Avoid => 1,
+        PrintConv    => 'Image::ExifTool::GPS::ToDMS($self, $val, 1, "E")',
+        PrintConvInv => 'Image::ExifTool::GPS::ToDegrees($val, 1, "lon")',
+    },
+    FlightXSpeed  => { Writable => 'real' },
+    FlightYSpeed  => { Writable => 'real' },
+    FlightZSpeed  => { Writable => 'real' },
+    CamReverse    => { }, # integer?
+    GimbalReverse => { }, # integer?
+    SelfData      => { Groups => { 2 => 'Image' } },
+    CalibratedFocalLength    => { Writable => 'real', Groups => { 2 => 'Image' } },
+    CalibratedOpticalCenterX => { Writable => 'real', Groups => { 2 => 'Image' } },
+    CalibratedOpticalCenterY => { Writable => 'real', Groups => { 2 => 'Image' } },
+    RtkFlag       => { }, # integer?
+    RtkStdLon     => { Writable => 'real' },
+    RtkStdLat     => { Writable => 'real' },
+    RtkStdHgt     => { Writable => 'real' },
+    DewarpData    => { Groups => { 2 => 'Image' } },
+    DewarpFlag    => { Groups => { 2 => 'Image' } }, # integer?
+    Latitude => {
+        Name => 'Latitude',
+        Writable => 'real',
+        PrintConv    => 'Image::ExifTool::GPS::ToDMS($self, $val, 1, "N")',
+        PrintConvInv => 'Image::ExifTool::GPS::ToDegrees($val, 1, "lat")',
+    },
+    Longitude => {
+        Name => 'Longitude',
+        Writable => 'real',
+        PrintConv    => 'Image::ExifTool::GPS::ToDMS($self, $val, 1, "E")',
+        PrintConvInv => 'Image::ExifTool::GPS::ToDegrees($val, 1, "lon")',
+    },
+);
+
+#------------------------------------------------------------------------------
+# Process DJI info (ref PH)
+# Inputs: 0) ExifTool ref, 1) dirInfo ref, 2) tag table ref
+# Returns: 1 on success
+sub ProcessDJIInfo($$$)
+{
+    my ($et, $dirInfo, $tagTbl) = @_;
+    my $dataPt = $$dirInfo{DataPt};
+    my $dirStart = $$dirInfo{DirStart} || 0;
+    my $dirLen = $$dirInfo{DirLen} || (length($$dataPt) - $dirStart);
+    if ($dirStart) {
+        my $buff = substr($$dataPt, $dirStart, $dirLen);
+        $dataPt = \$buff;
+    }
+    $et->VerboseDir('DJIInfo', undef, length $$dataPt);
+    while ($$dataPt =~ /\G\[(.*?)\](?=(\[|$))/sg) {
+        my ($tag, $val) = split /:/, $1, 2;
+        next unless defined $tag and defined $val;
+        if ($val =~ /^([\x20-\x7f]+)\0*$/) {
+            $val = $1;
+        } else {
+            my $buff = $val;
+            $val = \$buff;
+        }
+        if (not $$tagTbl{$tag} and $tag=~ /^[-_a-zA-Z0-9]+$/) {
+            my $name = $tag;
+            $name =~ s/_([a-z])/_\U$1/g;
+            AddTagToTable($tagTbl, $tag, { Name => Image::ExifTool::MakeTagName($name) });
+        }
+        $et->HandleTag($tagTbl, $tag, $val);
+    }
+    return 1;
+}
+
+__END__
+
+=head1 NAME
+
+Image::ExifTool::DJI - DJI Phantom maker notes tags
+
+=head1 SYNOPSIS
+
+This module is loaded automatically by Image::ExifTool when required.
+
+=head1 DESCRIPTION
+
+This module contains definitions required by Image::ExifTool to interpret
+the maker notes in images from some DJI Phantom drones.
+
+=head1 AUTHOR
+
+Copyright 2003-2023, Phil Harvey (philharvey66 at gmail.com)
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<Image::ExifTool::TagNames/DJI Tags>,
+L<Image::ExifTool(3pm)|Image::ExifTool>
+
+=cut
---
+++ exiftool/lib/Image/ExifTool/JSON.pm
@@ -0,0 +1,190 @@
+#------------------------------------------------------------------------------
+# File:         JSON.pm
+#
+# Description:  Read JSON files
+#
+# Notes:        Set ExifTool MissingTagValue to "null" to ignore JSON nulls
+#
+# Revisions:    2017/03/13 - P. Harvey Created
+#------------------------------------------------------------------------------
+
+package Image::ExifTool::JSON;
+use strict;
+use vars qw($VERSION);
+use Image::ExifTool qw(:DataAccess :Utils);
+use Image::ExifTool::Import;
+
+$VERSION = '1.05';
+
+sub ProcessJSON($$);
+sub ProcessTag($$$$%);
+
+%Image::ExifTool::JSON::Main = (
+    GROUPS => { 0 => 'JSON', 1 => 'JSON', 2 => 'Other' },
+    VARS => { NO_ID => 1 },
+    PROCESS_PROC => \&ProcessJSON,
+    NOTES => q{
+        Other than a few tags in the table below, JSON tags have not been
+        pre-defined.  However, ExifTool will read any existing tags from basic
+        JSON-formatted files.
+    },
+    # ON1 settings tags
+    ON1_SettingsData => {
+        RawConv => q{
+            require Image::ExifTool::XMP;
+            $val = Image::ExifTool::XMP::DecodeBase64($val);
+        },
+        SubDirectory => { TagTable => 'Image::ExifTool::PLIST::Main' },
+    },
+    ON1_SettingsMetadataCreated     => { Groups => { 2 => 'Time' } },
+    ON1_SettingsMetadataModified    => { Groups => { 2 => 'Time' } },
+    ON1_SettingsMetadataName        => { },
+    ON1_SettingsMetadataPluginID    => { },
+    ON1_SettingsMetadataTimestamp   => { Groups => { 2 => 'Time' } },
+    ON1_SettingsMetadataUsage       => { },
+    ON1_SettingsMetadataVisibleToUser=>{ },
+);
+
+#------------------------------------------------------------------------------
+# Store a tag value
+# Inputs: 0) ExifTool ref, 1) tag table, 2) tag ID, 3) value, 4) tagInfo flags
+sub FoundTag($$$$%)
+{
+    my ($et, $tagTablePtr, $tag, $val, %flags) = @_;
+
+    # special case to reformat ON1 tag names
+    if ($tag =~ s/^settings\w{8}-\w{4}-\w{4}-\w{4}-\w{12}(Data|Metadata.+)$/ON1_Settings$1/) {
+        $et->OverrideFileType('ONP','application/on1') if $$et{FILE_TYPE} eq 'JSON';
+    }
+
+    # avoid conflict with special table entries
+    $tag .= '!' if $Image::ExifTool::specialTags{$tag};
+
+    AddTagToTable($tagTablePtr, $tag, {
+        Name => Image::ExifTool::MakeTagName($tag),
+        %flags,
+        Temporary => 1,
+    }) unless $$tagTablePtr{$tag};
+
+    $et->HandleTag($tagTablePtr, $tag, $val);
+}
+
+#------------------------------------------------------------------------------
+# Process a JSON tag
+# Inputs: 0) ExifTool ref, 1) tag table, 2) tag ID, 3) value, 4) tagInfo flags
+# - expands structures into flattened tags as required
+sub ProcessTag($$$$%)
+{
+    local $_;
+    my ($et, $tagTablePtr, $tag, $val, %flags) = @_;
+
+    if (ref $val eq 'HASH') {
+        if ($et->Options('Struct')) {
+            FoundTag($et, $tagTablePtr, $tag, $val, %flags, Struct => 1);
+            return unless $et->Options('Struct') > 1;
+        }
+        # support hashes with ordered keys
+        my @keys = $$val{_ordered_keys_} ? @{$$val{_ordered_keys_}} : sort keys %$val;
+        foreach (@keys) {
+            my $tg = $tag . ((/^\d/ and $tag =~ /\d$/) ? '_' : '') . ucfirst;
+            $tg =~ s/([^a-zA-Z])([a-z])/$1\U$2/g;
+            ProcessTag($et, $tagTablePtr, $tg, $$val{$_}, %flags, Flat => 1);
+        }
+    } elsif (ref $val eq 'ARRAY') {
+        foreach (@$val) {
+            ProcessTag($et, $tagTablePtr, $tag, $_, %flags, List => 1);
+        }
+    } elsif (defined $val) {
+        FoundTag($et, $tagTablePtr, $tag, $val, %flags);
+    }
+}
+
+#------------------------------------------------------------------------------
+# Extract meta information from a JSON file
+# Inputs: 0) ExifTool object reference, 1) dirInfo reference
+# Returns: 1 on success, 0 if this wasn't a recognized JSON file
+sub ProcessJSON($$)
+{
+    local $_;
+    my ($et, $dirInfo) = @_;
+    my $raf = $$dirInfo{RAF};
+    my $structOpt = $et->Options('Struct');
+    my (%database, $key, $tag, $dataPt);
+
+    unless ($raf) {
+        $dataPt = $$dirInfo{DataPt};
+        if ($$dirInfo{DirStart} or ($$dirInfo{DirLen} and $$dirInfo{DirLen} ne length($$dataPt))) {
+            my $buff = substr(${$$dirInfo{DataPt}}, $$dirInfo{DirStart}, $$dirInfo{DirLen});
+            $dataPt = \$buff;
+        }
+        $raf = new File::RandomAccess($dataPt);
+        # extract as a block if requested
+        my $blockName = $$dirInfo{BlockInfo} ? $$dirInfo{BlockInfo}{Name} : '';
+        my $blockExtract = $et->Options('BlockExtract');
+        if ($blockName and ($blockExtract or $$et{REQ_TAG_LOOKUP}{lc $blockName} or
+            ($$et{TAGS_FROM_FILE} and not $$et{EXCL_TAG_LOOKUP}{lc $blockName})))
+        {
+            $et->FoundTag($$dirInfo{BlockInfo}, $$dataPt);
+            return 1 if $blockExtract and $blockExtract > 1;
+        }
+        $et->VerboseDir('JSON');
+    }
+
+    # read information from JSON file into database structure
+    my $err = Image::ExifTool::Import::ReadJSON($raf, \%database,
+        $et->Options('MissingTagValue'), $et->Options('Charset'));
+
+    return 0 if $err or not %database;
+
+    $et->SetFileType() unless $dataPt;
+
+    my $tagTablePtr = GetTagTable('Image::ExifTool::JSON::Main');
+
+    # remove any old tag definitions in case they change flags
+    foreach $key (TagTableKeys($tagTablePtr)) {
+        delete $$tagTablePtr{$key} if $$tagTablePtr{$key}{Temporary};
+    }
+
+    # extract tags from JSON database
+    foreach $key (sort keys %database) {
+        foreach $tag (sort keys %{$database{$key}}) {
+            my $val = $database{$key}{$tag};
+            # (ignore SourceFile if generated automatically by ReadJSON)
+            next if $tag eq 'SourceFile' and defined $val and $val eq '*';
+            ProcessTag($et, $tagTablePtr, $tag, $val);
+        }
+    }
+    return 1;
+}
+
+1;  # end
+
+__END__
+
+=head1 NAME
+
+Image::ExifTool::JSON - Read JSON files
+
+=head1 SYNOPSIS
+
+This module is used by Image::ExifTool
+
+=head1 DESCRIPTION
+
+This module contains definitions required by Image::ExifTool read
+information from JSON files.
+
+=head1 AUTHOR
+
+Copyright 2003-2023, Phil Harvey (philharvey66 at gmail.com)
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<Image::ExifTool::TagNames/JSON Tags>,
+L<Image::ExifTool(3pm)|Image::ExifTool>
+
+=cut
+
