2db8d592ad0aba303e8c9b71b0c5b26fa8c95a89
Leslie Van <viruswjc@gmail.com>
2023-04-23 18:37:29 +0800

    组织多级菜单，用于精简显示效果

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def processing():
     processor_chain = ProcessorChain()
 
     # 如果需要添加阴影，则添加阴影处理器，阴影处理器优先级最高，但是正方形布局不需要阴影
-    if config.is_shadow_enable() and 'square' != config.get_layout_type():
+    if config.has_shadow_enabled() and 'square' != config.get_layout_type():
         processor_chain.add(SHADOW_PROCESSOR)
 
     # 根据布局添加不同的水印处理器
@@ -44,14 +44,14 @@ def processing():
         processor_chain.add(WATERMARK_PROCESSOR)
 
     # 如果需要添加白边，且不是正方形布局，则添加白边处理器，白边处理器优先级最低
-    if config.is_white_margin_enable() and 'square' != config.get_layout_type():
+    if config.has_white_margin_enabled() and 'square' != config.get_layout_type():
         processor_chain.add(MARGIN_PROCESSOR)
 
     for source_path in tqdm(file_list):
         # 打开图片
         container = ImageContainer(source_path)
         # 添加logo
-        if config.is_logo_enable():
+        if config.has_logo_enabled():
             container.set_logo(config.load_logo(container.make))
         else:
             container.set_logo(None)
@@ -74,6 +74,8 @@ def processing():
 
 
 state = 0
+current_menu = root_menu
+root_menu.set_parent(root_menu)
 
 if __name__ == '__main__':
     while True:
@@ -81,11 +83,11 @@ if __name__ == '__main__':
         if state == 0:
             # 显示主菜单
             print(SEPARATE_LINE)
-            root_menu.display()
+            current_menu.display()
             print(SEPARATE_LINE)
 
             # 处理用户输入
-            user_input = input('输入【y 或回车】按照当前设置开始处理图片，输入【数字】修改设置，输入【x】退出程序\n')
+            user_input = input('输入【y 或回车】按照当前设置开始处理图片，输入【数字】修改设置，输入【r】返回上一层菜单，输入【x】退出程序\n')
 
             # y 或回车，跳转状态 100，开始处理图片
             if user_input == 'y' or user_input == '':
@@ -93,9 +95,15 @@ if __name__ == '__main__':
             # x 退出程序
             elif user_input == 'x' or user_input == 'X':
                 sys.exit(0)
+            # r 返回上一层
+            elif user_input == 'r' or user_input == 'R':
+                current_menu = current_menu.get_parent()
             # 数字合法则跳转到对应子菜单
-            elif user_input.isdigit() and 1 <= int(user_input) <= len(root_menu.components):
-                state = int(user_input)
+            elif user_input.isdigit() and 1 <= int(user_input) <= len(current_menu.components):
+                current_menu = current_menu.components[int(user_input) - 1]
+                if current_menu.is_leaf():
+                    current_menu.run()
+                    current_menu = root_menu
             else:
                 print('输入错误，请重新输入')
         elif state == 100:
@@ -105,26 +113,5 @@ if __name__ == '__main__':
         elif state == -1:
             # 退出程序
             sys.exit(0)
-        else:
-            # 显示子菜单
-            submenu = root_menu.components[state - 1]
-            print(SEPARATE_LINE)
-            submenu.display()
-            print(SEPARATE_LINE)
-
-            # 处理用户输入
-            user_input = input('输入【数字】修改设置，输入【x】退出程序，输入【r】返回主菜单\n')
-
-            # x 退出程序
-            if user_input == 'x' or user_input == 'X':
-                sys.exit(0)
-            # 数字合法则执行对应菜单项的处理函数
-            elif user_input == 'r' or user_input == 'R':
-                state = 0
-            elif user_input.isdigit() and 1 <= int(user_input) <= len(submenu.components):
-                submenu.components[int(user_input) - 1].run()
-                state = 0
-            else:
-                print('输入错误，请重新输入')
         # 保存配置
         config.save()
