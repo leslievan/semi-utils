b9fcf029e67455c9db7b9f591007548376a910ad
Leslie Van <viruswjc@gmail.com>
2023-05-05 06:07:26 +0800

    - 修改字体颜色配置

--- entity/config.py
+++ entity/config.py
@@ -29,7 +29,10 @@ class ElementConfig(object):
         return self.element['value'] if 'value' in self.element else None
 
     def get_color(self):
-        return self.element['color'] if 'color' in self.element else '#000000'
+        if 'color' in self.element:
+            return self.element['color']
+        else:
+            return self.element['color']
 
 
 # 字体大小，影响字体的清晰度
