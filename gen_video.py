9ef14bf993d079551d3e96bf0e0a4c32e16a5dd3
Leslie Van <viruswjc@gmail.com>
2023-08-05 18:10:06 +0800

    append generate_video func #

---
+++ gen_video.py
@@ -0,0 +1,51 @@
+import os
+import platform
+import subprocess
+from datetime import datetime
+
+
+def get_ffmpeg_path():
+    if platform.system() == "Windows":
+        if os.path.exists("./bin/ffmpeg.exe"):
+            return "./bin/ffmpeg.exe"
+        # 尝试使用 where 获取 ffmpeg 路径
+        ffmpeg_path = subprocess.getoutput("where ffmpeg")
+        if "ffmpeg" in ffmpeg_path:
+            return ffmpeg_path.split('\n')[0]  # 取第一个结果
+        return None
+    else:
+        ffmpeg_path = subprocess.getoutput("which ffmpeg")
+        if "ffmpeg" in ffmpeg_path:
+            return ffmpeg_path
+        elif os.path.exists("./bin/ffmpeg"):
+            return "./bin/ffmpeg"
+        return None
+
+
+def generate_video(path, gap_time=2):
+    ffmpeg_path = get_ffmpeg_path()
+    if not ffmpeg_path:
+        print("请先安装ffmpeg!")
+        return
+
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    output_file = os.path.join(path, f"{current_time}.mp4")
+    command = f'{ffmpeg_path} -framerate 1/{gap_time} -pattern_type glob -i "{path}/*.jpg" -vf "scale=3840:2160:force_original_aspect_ratio=decrease,pad=3840:2160:(ow-iw)/2:(oh-ih)/2:color=white" -c:v libx264 -r 24 -pix_fmt yuv420p -color_range 1 {output_file}'
+
+    subprocess.call(command, shell=True)
+
+    # 检查是否存在 bgm.mp3 文件
+    bgm_path = os.path.join(path, "bgm.mp3")
+    if os.path.exists(bgm_path):
+        temp_output_file = os.path.join(path, f"temp_{current_time}.mp4")
+        command_bgm = f'{ffmpeg_path} -i {output_file} -i {bgm_path} -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 -shortest {temp_output_file}'
+        subprocess.call(command_bgm, shell=True)
+        # 重命名临时文件回原文件名
+        os.rename(temp_output_file, output_file)
+
+    # 提示视频生成成功，告诉用户视频的路径
+    print(f"视频生成成功，路径为：{output_file}")
+
+
+if __name__ == '__main__':
+    generate_video("./output")
