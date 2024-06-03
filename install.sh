6a0e4d85231b36746d9555122aaca7e47182bd43
Floyd Li <floyd.li@outlook.com>
2024-06-03 10:54:35 +0800

    chore: update exiftool download

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ curl -O https://exiftool.org/Image-ExifTool-12.85.tar.gz
 mkdir -p ./exiftool
 
 # 解压文件
-tar -xzf Image-ExifTool-12.60.tar.gz -C ./exiftool --strip-components=1
+tar -xzf Image-ExifTool-12.85.tar.gz -C ./exiftool --strip-components=1
 
 # 删除压缩包
-rm Image-ExifTool-12.60.tar.gz
+rm Image-ExifTool-12.85.tar.gz
 
 # 下载 python 依赖
 pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
