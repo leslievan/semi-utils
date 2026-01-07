bd223c2b1658c9eb158da53ac462d473e109098f
Leslie Van <viruswjc@gmail.com>
2026-01-07 20:57:29 +0800

    feat: 添加 heic 支持

--- processor/__init__.py
+++ processor/__init__.py
@@ -1,9 +1,12 @@
+import pillow_heif
+
 from processor.filters import BlurFilter, MarginFilter, ResizeFilter, TrimFilter, WatermarkFilter, \
     WatermarkWithTimestampFilter
 from processor.generators import SolidColorGenerator, GradientColorGenerator, RichTextGenerator, MultiRichTextGenerator
 from processor.mergers import ConcatMerger
 
 if __name__ == '__main__':
+    # 注册处理器
     BlurFilter()
     MarginFilter()
     ResizeFilter()
@@ -15,3 +18,6 @@ if __name__ == '__main__':
     MultiRichTextGenerator()
     ConcatMerger()
     WatermarkWithTimestampFilter()
+
+    # 注册 pillow heic 支持
+    pillow_heif.register_heif_opener()
