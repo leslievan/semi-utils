df9ea87e3658ee7ef7eff17b49e94e8f8fd68cbd
Leslie Van <viruswjc@gmail.com>
2026-01-08 21:51:07 +0800

    feat: 修复无法处理 heic 文件的问题

--- processor/__init__.py
+++ processor/__init__.py
@@ -5,19 +5,19 @@ from processor.filters import BlurFilter, MarginFilter, ResizeFilter, TrimFilter
 from processor.generators import SolidColorGenerator, GradientColorGenerator, RichTextGenerator, MultiRichTextGenerator
 from processor.mergers import ConcatMerger
 
-if __name__ == '__main__':
-    # 注册处理器
-    BlurFilter()
-    MarginFilter()
-    ResizeFilter()
-    TrimFilter()
-    WatermarkFilter()
-    SolidColorGenerator()
-    GradientColorGenerator()
-    RichTextGenerator()
-    MultiRichTextGenerator()
-    ConcatMerger()
-    WatermarkWithTimestampFilter()
+# 注册处理器
+BlurFilter()
+MarginFilter()
+ResizeFilter()
+TrimFilter()
+WatermarkFilter()
+SolidColorGenerator()
+GradientColorGenerator()
+RichTextGenerator()
+MultiRichTextGenerator()
+ConcatMerger()
+WatermarkWithTimestampFilter()
 
-    # 注册 pillow heic 支持
-    pillow_heif.register_heif_opener()
+# 注册 pillow heic 支持
+pillow_heif.register_heif_opener()
+print("Registered plugin: pillow_heif")
