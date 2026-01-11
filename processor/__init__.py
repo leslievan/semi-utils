38cd864969f7529e679dba7ffe397b7fe378a69a
Leslie Van <viruswjc@gmail.com>
2026-01-11 18:34:36 +0800

    feat: 注册阴影等处理器

--- processor/__init__.py
+++ processor/__init__.py
@@ -1,8 +1,9 @@
 import pillow_heif
 
 from processor.filters import BlurFilter, MarginFilter, ResizeFilter, TrimFilter, WatermarkFilter, \
-    WatermarkWithTimestampFilter
-from processor.generators import SolidColorGenerator, GradientColorGenerator, RichTextGenerator, MultiRichTextGenerator
+    WatermarkWithTimestampFilter, ShadowFilter, RoundedCornerFilter
+from processor.generators import SolidColorGenerator, GradientColorGenerator, RichTextGenerator, MultiRichTextGenerator, \
+    ImageLoader
 from processor.mergers import ConcatMerger
 
 # 注册处理器
@@ -17,6 +18,9 @@ RichTextGenerator()
 MultiRichTextGenerator()
 ConcatMerger()
 WatermarkWithTimestampFilter()
+ShadowFilter()
+ImageLoader()
+RoundedCornerFilter()
 
 # 注册 pillow heic 支持
 pillow_heif.register_heif_opener()
