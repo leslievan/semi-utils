9de78691890c65354060db5e4ef4b9a48c616edd
Leslie Van <viruswjc@gmail.com>
2026-01-11 17:40:47 +0800

    feat: 优化依赖列表

--- core/configs.py
+++ core/configs.py
@@ -1,5 +1,6 @@
 import configparser
-import tomllib
+
+import tomli
 
 from core import CONFIG_PATH, PROJECT_INFO
 
@@ -12,5 +13,5 @@ def load_config() -> configparser.ConfigParser:
 
 def load_project_info():
     with open(PROJECT_INFO, "rb") as f:  # 注意：tomllib 需要以二进制模式（"rb"）打开文件
-        data = tomllib.load(f)
+        data = tomli.load(f)
     return data
