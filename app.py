51a96dbb6c9793522e2665a4caee6fc7f65da960
Leslie Van <viruswjc@gmail.com>
2026-01-13 03:09:37 +0800

    feat: 添加files上下文

--- app.py
+++ app.py
@@ -181,6 +181,7 @@ def handle_process():
             'filename': _input_path.stem,
             'file_dir': str(_input_path.parent.absolute()).replace('\\', '/'),
             'file_path': str(_input_path).replace('\\', '/'),
+            'files': input_files
         }
         final_template = template.render(context)
         llogger.debug(f'''
